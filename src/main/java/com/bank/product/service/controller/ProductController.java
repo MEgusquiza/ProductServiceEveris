5e1a713ea844dadeb3d4257a1d0f70ce67bbc695
MEgusquiza <85919017+MEgusquiza@users.noreply.github.com>
2021-07-06 17:19:44 -0500

    Update ProductController.java

--- src/main/java/com/bank/product/service/controller/ProductController.java
+++ src/main/java/com/bank/product/service/controller/ProductController.java
@@ -1,6 +1,9 @@
 package com.bank.product.service.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,52 +11,52 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bank.product.service.domain.dto.ProductDTO;
 import com.bank.product.service.domain.service.ProductService;
-import com.bank.product.service.persistence.entity.Product;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-//@RestController
-//@RequestMapping("/product")
+@RestController
+@RequestMapping("/product")
 public class ProductController {
 
-/*
-	@Autowired
-	private ProductService<Product> productService;
+  @Autowired
+  private ProductService productService;
 
-	@GetMapping
-	public Flux<Product> getAllProducts(){
-		return productService.findAll();
-	}
-	
-	@GetMapping("/{id}")
-	public Mono<Product> getProduct(@PathVariable String id){
-		return productService.findEntityById(id);
-	}
-	
-	@PostMapping
-	public Mono<Product> saveProduct(@RequestBody Product transactionMono) throws Exception{
-		return productService.createEntity(transactionMono);
-	}
-	
-	@PutMapping
-	public Mono<Product> updateProduct(@RequestBody Product transactionMono){
-		return productService.updateEntity(transactionMono);
-	}
-	
-	@DeleteMapping("/{id}")
-	public Mono<Void> deleteProduct(@PathVariable String id){
-		return productService.deleteEntity(id);
-	}
-	*/
-	/*@GetMapping("/find-by-product/{idProduct}")
-	public Mono<Product> getTransacionByIdProduct(@PathVariable String idProduct){
-		return productService.getProductByIdProduct(idProduct);
-	}*/
-	
-
+  @PostMapping(value = { "/save", "/" })
+  @ResponseStatus(HttpStatus.OK)
+  public Mono<ProductDTO>  createProduct(@RequestBody Mono<ProductDTO> productDto) {
+   return productService.create(productDto);
+  }
+  
+  @GetMapping(value = "/{id}")
+  @ResponseStatus(HttpStatus.OK)
+  public ResponseEntity<Mono<ProductDTO>> findProductById(@PathVariable("id") String id) {
+      Mono<ProductDTO> e = productService.findById(id);
+      HttpStatus status = (e != null) ? HttpStatus.OK : HttpStatus.NOT_FOUND;
+      return new ResponseEntity<>(e, status);
+  }
+  
+  @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+  @ResponseStatus(HttpStatus.OK)
+  public Flux<ProductDTO> findAllProduct() {
+      return productService.findAll();
+  }
+  
+  @PutMapping(value = "/{id}")
+  @ResponseStatus(HttpStatus.OK)
+  public Mono<ProductDTO> update(@PathVariable("id") String id,@RequestBody Mono<ProductDTO> e) {
+      return productService.update(id, e);
+  }
+  
+  @DeleteMapping(value = "/delete/{id}")
+  @ResponseStatus(HttpStatus.OK)
+  public void delete(@PathVariable("id") String id) {
+    productService.deleteById(id).subscribe();
+  }
 	
 }
